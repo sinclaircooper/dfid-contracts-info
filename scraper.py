d5bb2bdcea5f2af35af97a33301771b5b891d541
sinclaircooper <sinclairscooper@gmail.com>
2014-04-30 12:44:11 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -46,7 +46,7 @@ for link in links:
 
             elif span_text == "Awarded value":
                 #print info
-                value = int(info_decoded.encode('utf8')replace(',','').replace('£',''))
+                value = int(info_decoded.encode('utf8')replace(u',','').replace(u'£',''))
                 #print value
 
             elif span_text == "Location where the contract is to be carried out:":
